c11b9bde234f912c17c1fbe9acd2871bd0f39880
Valiana <valiana.josic@gmail.com>
2020-05-17 14:42:37 -0400

    added nav bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './Pages/Home/home';
 import Contact from './Pages/Contact/contact'
-
+import Navigation from './Components/Nav/nav';
 
 
 class App extends Component {
@@ -12,6 +12,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App Site">
           <div className="Site-content">
+          <Navigation />
             <Switch>
               <Route path="/" component={Home} exact />
               <Route path="/contact" component={Contact} />
---
+++ src/Components/Nav/nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './nav.css';
+import Logo from '../../Assets/Images/logo1.png';
+import { NavLink } from 'react-router-dom';
+
+
+const Navigation = () => {
+  return (
+    <div className="nav nav-div">
+      <NavLink to="/" className="nav-page logo-link"> <img src={Logo} alt="logo" className="logo" /></NavLink>
+      <div className="link-div">
+        <NavLink to="/properties" className="nav-page nav-link property-link"><span className="span-btn">Properties</span></NavLink>
+        <NavLink to="/about" className="nav-page nav-link"><span  className="span-btn">About</span></NavLink>
+        <NavLink to="/contact" className="nav-page nav-link"><span  className="span-btn">Contact</span></NavLink>
+      </div>
+      <div className="icon-bar">
+        <a href="https://www.facebook.com/Thais-Reyes-357850721490279/" target="_blank" rel="noopener  noreferrer" className="facebook"><i className="fa fa-facebook"></i></a>
+        <a href="https://www.linkedin.com/in/thais-reyes-%F0%9F%8F%96-43474526" target="_blank" rel="noopener  noreferrer" className="twitter"><i className="fa fa-twitter"></i></a>
+        <a href="https://www.linkedin.com/in/thais-reyes-%F0%9F%8F%96-43474526" target="_blank" rel="noopener  noreferrer" className="linkedin"><i className="fa fa-linkedin"></i></a>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
