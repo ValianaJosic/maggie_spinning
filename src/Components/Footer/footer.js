f4aa7c67e373089d0ceef619fe3b02e09a85f378
Valiana <valiana.josic@gmail.com>
2020-05-19 17:19:33 -0400

    added footer

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer = () => {
+  return (
+
+    <div className="footer">
+      <div>Maggie Mijares</div>
+      <div>Coach / Spinning Instructor</div>
+      <div>Maggie@gmail.com</div>
+      <div>	&copy;</div>
+    </div>
+  );
+}
+
+export default Footer;
