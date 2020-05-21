e5fa5781ceeba8a5b0ad94b3c2cfed38f0fac0fb
Valiana <valiana.josic@gmail.com>
2020-05-21 15:50:59 -0400

    Hero image for home page

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './home.css';
 
+import Spin from '../../Assets/Images/spin.jpg'
+
 
 function Home() {
     
   return (
     <div>
-     
+      <img alt= "spin" src={Spin} className="hero"></img>
     </div>
   );
 }
