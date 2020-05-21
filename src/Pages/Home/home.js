25330cfe578627b24daf7aca9e853f99ecc3da29
Valiana <valiana.josic@gmail.com>
2020-05-21 15:54:55 -0400

    Home page enhancements title

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -5,11 +5,19 @@ import Spin from '../../Assets/Images/spin.jpg'
 
 
 function Home() {
-    
+
   return (
     <div>
-      <img alt= "spin" src={Spin} className="hero"></img>
+      <img alt="spin" src={Spin} className="hero"></img>
+      <div>
+        <p>COACH</p>
+        <p>Caracas,Venezuela</p>
+        <p>Premium weight loss and lifestyle transformations which create long lasting, dramatic results to your health, body & mind.
+        </p>
+
+      </div>
     </div>
+
   );
 }
 
