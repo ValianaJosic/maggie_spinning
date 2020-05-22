b5907cfb3d22a567d67b222ce4664b9a77af1482
Valiana <valiana.josic@gmail.com>
2020-05-22 16:10:13 -0400

    Contact form

--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -1,14 +1,53 @@
-import React from 'react';
 import './contact.css';
 
+import React from "react";
 
-const Contact = () => {
-  return (
+export default class contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
 
-    <div >
-      HI
-    </div>
-  );
+  render() {
+    const { status } = this.state;
+    return (
+      <form
+        onSubmit={this.submitForm}
+        action="https://formspree.io/xlepgyen"
+        method="POST"
+        className="form"
+      >
+         <label className='form-label'>Name</label>
+        <input className='form-input'type="text" name="name" />
+        <label  className='form-label'>Email</label>
+        <input className='form-input' type="email" name="email" />
+        <label  className='form-label'>Message</label>
+        <textarea className='form-area' type="textarea" name="message" />
+        {status === "SUCCESS" ? <p>Thanks!</p> : <button className='form-btn'>Submit</button>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </form>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
 }
-
-export default Contact;
