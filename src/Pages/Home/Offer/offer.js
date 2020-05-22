cd41183c164fccd73ef58b699f1051a587ab21e0
Valiana <valiana.josic@gmail.com>
2020-05-22 14:07:23 -0400

    CSS home

--- src/Pages/Home/Offer/offer.js
+++ src/Pages/Home/Offer/offer.js
@@ -6,11 +6,37 @@ import img from '../../../Assets/Images/spin.jpeg'
 
 const Offer = () => {
   return (
-    
+
     <div className="offer">
-        <div className="offer-div">
-            <img alt=' ' src={img} className="offer-img"></img>
-        </div>
+      <div className="offer-info">
+        <p className="home-title">
+          Nutrition
+        </p>
+        <p className="offer-about">
+          Learn about and implement nutritional strategies that will transform you.
+        </p>
+        <p className="home-title">
+          Exercise
+        </p>
+        <p className="offer-about">
+          Tailored, structured, time-efficient & progressive exercise programme.
+        </p>
+        <p className="home-title">
+          Lifestyle
+        </p>
+        <p className="offer-about">  
+          Learn about and implement vital lifestyle habits to start feeling amazing on the inside and looking great on the outside.
+        </p>
+        <p className="home-title">
+          Mindset
+        </p>
+        <p className="offer-about">  
+          By applying mindset principles to your burning desire of achieving your goals - you will become a better version of you.
+        </p>
+      </div>
+      <div className="offer-div">
+        <img alt=' ' src={img} className="offer-img"></img>
+      </div>
     </div>
   );
 }
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,32 +1,35 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './home.css';
 
 import Spin from '../../Assets/Videos/spin.mp4'
 import Offer from '../Home/Offer/offer'
+import Youtube from '../Home/Youtube/youtube'
 
 
 function Home() {
 
   return (
-    <Fragment>
-    <div className='home'>
-        <video autoPlay="autoplay" loop="loop" muted className="video hero">
-          <source src={Spin} type="video/mp4" />
+
+      <div className='home'>
+        <div className="home-intro">
+        <div className="video hero">
+          <video autoPlay="autoplay" loop="loop" muted >
+            <source src={Spin} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      <div className="home-info">
-        <p className="home-title">COACH</p>
-        <p>Caracas, Venezuela</p>
-        <p>Premium weight loss and lifestyle transformations</p>
-        <p> which create long lasting,</p>
-        <p> dramatic results to your health, body & mind.
+        </div>
+        <div className="home-info">
+          <p className="home-title">COACH</p>
+          <p>Caracas, Venezuela</p>
+          <p>Premium weight loss and lifestyle transformations</p>
+          <p> which create long lasting,</p>
+          <p> dramatic results to your health, body & mind.
         </p>
+        </div>
+        </div>
+      <Offer />
+      <Youtube/>
       </div>
-    </div>
-    <div>
-    <Offer/>
-    </div>
-    </Fragment>
   );
 }
 
