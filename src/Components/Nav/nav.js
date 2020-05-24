b96f6ec3da7c42cb9f574af40a502356f0a8b5f9
Valiana <valiana.josic@gmail.com>
2020-05-24 15:28:31 -0400

    Edit NAvlink

--- src/Components/Nav/nav.js
+++ src/Components/Nav/nav.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './nav.css';
 import Logo from '../../Assets/Images/logo1.png';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 
 const Navigation = () => {
   return (
     <div className="nav nav-div">
-      <NavLink to="/" className="nav-page logo-link"> <img src={Logo} alt="logo" className="logo" /></NavLink>
+      <a href="/" className="nav-page logo-link"> <img src={Logo} alt="logo" className="logo" /></a>
       <div className="link-div">
-        <NavLink to="/spinning" className="nav-page nav-link property-link"><span className="span-btn">Spinning</span></NavLink>
-        <NavLink to="/coach" className="nav-page nav-link"><span  className="span-btn">Coach</span></NavLink>
-        <NavLink to="/contact" className="nav-page nav-link"><span  className="span-btn">Contact</span></NavLink>
+        <a href="/spinning" className="nav-page nav-link property-link"><span className="span-btn">Spinning</span></a>
+        <a href="/coach" className="nav-page nav-link"><span  className="span-btn">Coach</span></a>
+        <a href="/contact" className="nav-page nav-link"><span  className="span-btn">Contact</span></a>
       </div>
       <div className="icon-bar">
         <a href="https://www.facebook.com/Thais-Reyes-357850721490279/" target="_blank" rel="noopener  noreferrer" className="facebook"><i className="fa fa-facebook"></i></a>
